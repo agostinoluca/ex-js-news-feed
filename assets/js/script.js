1f20dda8b162f47606fd8fa06765c64021525e02
agostinoluca <agostino.luca89@gmail.com>
2023-12-20 20:22:23 +0100

    create folder add files add links to html add css common rules add title and favicon add track of the exercise

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+/* 
+Step 1: struttura dei dati.
+Partendo dai dati forniti crea le strutture dati necessarie sfruttando array e oggetti facendo attenzione agli attributi che caratterizzano ciascuna news.
+
+Alcune note:
+●	nel definire una immagine servirà un url e non dimenticare di valorizzare l’attributo alt;
+●	modificare/aggiungere dati a vostro piacimento per arricchire l’esperienza.
+
+
+Step 2 - Stampa dei dati in pagina.
+Stampa in pagina le news del nostro feed utilizzando JavaScript.
+
+Alcune note:
+●	creare un secondo array in cui conservare la lista degli elementi salvati;
+●	per l’icona di salvataggio utilizzare FontAwesome o un semplice file svg;
+●	agganciare l’evento al tasto bookmark;
+●	ogni volta che elimini un elemento dal DOM tutti gli eventi ad esso associati vengono rimossi, trova un modo per riagganciarli;
+●	una volta salvata una news non è più possibile rimuoverla o cliccare nuovamente sull’icona.
+
+
+#Tools:
+-console.log();
+
+*/
+
+console.log('Hello World');
